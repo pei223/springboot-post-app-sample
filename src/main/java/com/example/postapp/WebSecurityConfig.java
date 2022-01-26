4b8c9a8de1f14a9f9cc5801847cb5ff84bc3d29f
pei223 <ippei.223miyake119@gmail.com>
2022-01-26 21:09:15 +0900

    認証必須パス修正

--- src/main/java/com/example/postapp/WebSecurityConfig.java
+++ src/main/java/com/example/postapp/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**", "/api/posts/", "/api/posts/{id:[\\\\d+]}", "/swagger" +
+                .authorizeRequests().antMatchers("/api/auth/**", "/api/posts/", "/api/posts/{id:\\d+}", "/swagger" +
                 "-ui/**", "/v3" +
                 "/**")
                 .permitAll()
